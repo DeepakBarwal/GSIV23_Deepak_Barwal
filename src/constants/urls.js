452a4c7cdac2228473556cbcd672eb27e0512e18
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-15 14:15:59 +0530

    Added constant urls file

---
+++ src/constants/urls.js
@@ -0,0 +1,2 @@
+// Example URL -> 'https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&primary_release_date.gte=2023-08-15&sort_by=popularity.desc';
+const API_BASE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`;
